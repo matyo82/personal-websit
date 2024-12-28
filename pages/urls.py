faaba31f1a323b5e46887b28a315985dd5533a15
matyo <109252920+matyo82@users.noreply.github.com>
2024-12-28 12:53:42 +0330

    cheng setting and use moredjango

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
-    path('about', views.about_page_view, name='about'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 def home_page_view(request):
-    return HttpResponse("Homepage")
-
-def about_page_view(request):
-    context = {
-        "name": "Alice",
-        "age": 22,
+    context = {  # new
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING.",
     }
-    return render(request, "pages/about.html", context)
+    return render(request, 'home.html', context)
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
+
